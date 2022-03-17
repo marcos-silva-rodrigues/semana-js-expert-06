48a8dcc6e51def50a7e887d042a76a745cfd8cae
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2022-03-16 21:29:47 -0300

    test: ajustando teste com correção

--- tests/unit/server/service.test.js
+++ tests/unit/server/service.test.js
@@ -2,8 +2,8 @@ import { jest, describe, test, expect, beforeEach } from "@jest/globals";
 import config from "../../../server/config";
 import { Service } from "../../../server/service";
 import TestUtil from "../_util/testUtil";
-import path from 'path';
 import fs from 'fs';
+import fsPromises from 'fs/promises';
 
 const {dir: { publicDirectory }, pages, location } = config;
 
@@ -14,50 +14,75 @@ describe("#Service", () => {
   });
 
   test("createFileStream", async () => {
-    const expectPath = `${publicDirectory}\\home\\index.html`;
     const streamMock = TestUtil.generateReadableStream(['data stream']);
+    const file = `index.html`;
     
-    jest.spyOn(fs, "createReadStream").mockReturnValue(streamMock);
+    jest
+      .spyOn(fs, fs.createReadStream.name)
+      .mockReturnValue(streamMock);
 
     const service = new Service();
-    const data = await service.createFileStream(expectPath);
+    const data = await service.createFileStream(file);
 
-    expect(data).toEqual(streamMock);
+    expect(data).toStrictEqual(streamMock);
+    expect(fs.createReadStream).toBeCalledWith(file);
   });
 
-  test("getFileInfo with valid file", async () => {
-    const expectPath = `${publicDirectory}\\home\\index.html`
-    const expectType = '.html';
+  test("getFileInfo", async () => {
+    jest.spyOn(
+      fsPromises,
+      fsPromises.access.name
+    ).mockResolvedValue()
 
-    jest.spyOn(path, "join").mockReturnValue(expectPath);
+    const currentSong = 'mySong.mp3'
+    const service = new Service()
+    const result = await service.getFileInfo(currentSong)
+    const expectedResult = {
+      type: '.mp3',
+      name: `${publicDirectory}\\${currentSong}`
+    }
 
-      const service = new Service();
-      const data = await service.getFileInfo(pages.homeHTML);
-
-      expect(data.type).toEqual(expectType);
-      expect(data.name).toEqual(expectPath);
+    expect(result).toStrictEqual(expectedResult)
   });
 
   test("getFileStream", async () => {
-    const streamMock =TestUtil.generateReadableStream(['index.html']);
-    const expectType = ".html";
+    const currentReadable = TestUtil.generateReadableStream(['abc']);
+    const currentSong = `mySong.mp3`;
+    const currentSongFullPath = `${publicDirectory}/${currentSong}`;
 
-    jest
-      .spyOn(Service.prototype, Service.prototype.createFileStream.name)
-      .mockReturnValue(streamMock);
-
-    jest
-      .spyOn(Service.prototype, Service.prototype.getFileInfo.name)
-      .mockReturnValue({
-        name: pages.homeHTML,
-        type: expectType
-      });
+    const fileInfo = {
+      type: '.mp3',
+      name: currentSongFullPath
+    };
 
     const service = new Service();
+    jest
+      .spyOn(
+        service,
+        service.getFileInfo.name
+      )
+      .mockResolvedValue(fileInfo);
 
-    const data = await service.getFileStream(location.home);
-    expect(Service.prototype.getFileInfo).toBeCalledWith(location.home);
-    expect(data.stream).toStrictEqual(streamMock);
-    expect(data.type).toStrictEqual(expectType);
+    jest
+      .spyOn(
+        service,
+        service.createFileStream.name
+      )
+      .mockReturnValue(currentReadable);
+
+    const result = await service.getFileStream(currentSong);
+    const expectedResult = {
+      type: fileInfo.type,
+      stream: currentReadable
+    };
+    expect(result).toStrictEqual(expectedResult)
+    expect(service.createFileStream).toHaveBeenCalledWith(
+      fileInfo.name
+    );
+
+    expect(service.getFileInfo).toHaveBeenCalledWith(
+      currentSong
+    );
+
   });
 });
